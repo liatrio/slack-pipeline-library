47dd6ad2076350f546e17a2298d29384b0dbd0cf
H607678 <jeffg@liatrio.com>
2019-04-22 21:51:57 -0700

    finding correct api endpoint

--- vars/sendPipelineFailure.groovy
+++ vars/sendPipelineFailure.groovy
@@ -4,6 +4,11 @@ import org.SlackPipeline.SlackPipeline
 def call(sp) {
 
   def blue_ocean_url = "${env.JENKINS_URL}blue/rest/organizations/jenkins/pipelines"
+  def job_path = "/" + env.BUILD_URL.replaceAll("${env.JENKINS_URL}", "")
+  job_path = job_path.replace("job/", "")
+  job_path = job_path.replaceAll("job", "pipelines")
+  blue_ocean_url += job_path + "steps/"
+  println(blue_ocean_url)
   //def stages = sh(returnStdout: true, script: "curl --silent  -H 'Authorization: Bearer ${env.SLACK_TOKEN}'  ${blue_ocean_url}").trim() 
 
   //def payload = sp.sendPipelineFailure("${env.STAGE_NAME}")
